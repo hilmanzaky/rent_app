4d119d17875c31141e0ca20ca9b39b363ab3d64d
Hilman Zaky <hilman@hilmans-soulmate.(none)>
2012-05-06 12:17:26 +0700

    add terbilang function to display sub total in Indonesian statements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,15 @@ module ApplicationHelper
   def iconed_text(icon, text)
     "<span class='#{icon}'></span>#{text}".html_safe
   end
+
+  def terbilang(angka)
+    satuan = [''] + %w{satu dua tiga empat lima enam tujuh delapan sembilan sepuluh sebelas}.collect{|x|x+' '}
+    triple = [['puluh',10,20,100],['ratus',100,200,1000],['ribu',1000,2000,1000000],['juta',1000000,1000000,1000000000],['milyar',1000000000,1000000000,1000000000000]]
+    hasil = satuan[angka] if angka<12
+    hasil = terbilang(angka.divmod(10)[1]) + 'belas ' if angka>=12 and angka<20
+    hasil = 'seratus '+terbilang(angka-100) if angka>=100 and angka<200
+    hasil = 'seribu ' +terbilang(angka-1000) if angka>=1000 and angka<2000
+    triple.each{|x|hasil = terbilang(angka.divmod(x[1])[0])+x[0]+' '+terbilang(angka.divmod(x[1])[1]) if angka>=x[2] and angka<x[3]}
+    hasil
+  end
 end
